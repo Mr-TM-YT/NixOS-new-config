38bac0eab900b403c395289ce5db9cf196a40f97
Mr-TM-YT <www.mohamedsalwa13@gmail.com>
2024-02-01 11:55:50 +0200

    Cleaned nixos/pkgs.nix

--- nixos/pkgs.nix
+++ nixos/pkgs.nix
@@ -34,14 +34,7 @@
     networkmanagerapplet
     qgnomeplatform
     qgnomeplatform-qt6
-    adwaita-qt
-    adwaita-qt6
     sddm-theme
-    libsForQt5.qtstyleplugin-kvantum
-    libsForQt5.qt5ct
     udiskie
-    qt5.qtsvg
-    qt5.qtgraphicaleffects
-    qt5.qtquickcontrols2
   ];
 }
